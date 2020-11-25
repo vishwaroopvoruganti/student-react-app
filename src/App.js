8e399d7769f4a4e0f01afd7faaf9c0d786deb4f3
Vishwaroop Voruganti <vishwaroopvoruganti@gmail.com>
2020-11-24 21:35:09 -0600

    first

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ const asyncReactiveForms = asyncComponent(() => {
 function initializeReactGA() {
   // console.log('Analytics');
   ReactGA.initialize("UA-155426348-1");
-  
   ReactGA.pageview('/');
   //ReactGA.pageview(window.location.pathname+window.location.search);
 }
